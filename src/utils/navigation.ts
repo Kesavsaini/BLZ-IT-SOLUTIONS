c2bdb3886bae2f65db9e5b032584c072cba96f8f
Keshav <keshavsainiofficial@gmail.com>
2024-12-30 13:56:06 +0530

    Update navigation links to include base path for services and contact

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,10 +1,10 @@
 // An array of links for navigation bar
 const navBarLinks = [
-  { name: "Home", url: "/" },
+  { name: "Home", url: "/BLZ-IT-SOLUTIONS/" },
   // { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
+  { name: "Services", url: "/BLZ-IT-SOLUTIONS/services" },
   // { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
+  { name: "Contact", url: "/BLZ-IT-SOLUTIONS/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
@@ -13,7 +13,7 @@ const footerLinks = [
     links: [
       // { name: "Documentation", url: "/welcome-to-docs/" },
       { name: "Our Products", url: "#" },
-      { name: "Our Services", url: "/services" },
+      { name: "Our Services", url: "/BLZ-IT-SOLUTIONS/services" },
     ],
   },
   {

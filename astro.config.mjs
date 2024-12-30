58ee9c07f68a32892dc9aa429ca177e284535486
Keshav <keshavsainiofficial@gmail.com>
2024-12-30 16:14:30 +0530

    Comment out Fillout script in ContactSection and update Astro configuration for social images

--- astro.config.mjs
+++ astro.config.mjs
@@ -91,14 +91,14 @@ export default defineConfig({
           tag: "meta",
           attrs: {
             property: "og:image",
-            content: "https://screwfast.uk" + "/social.webp",
+            // content: "https://screwfast.uk" + "/social.webp",
           },
         },
         {
           tag: "meta",
           attrs: {
             property: "twitter:image",
-            content: "https://screwfast.uk" + "/social.webp",
+            // content: "https://screwfast.uk" + "/social.webp",
           },
         },
       ],
